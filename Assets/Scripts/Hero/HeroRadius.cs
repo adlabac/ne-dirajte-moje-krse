ec24d8d6d31cb8a3712ffe4fe234bd4f5fba8a38
Branko Dujović <brankod@t-com.me>
2016-03-31 19:08:06 +0200

    Pravilno crtanje circle collidera koji se odnosi na domet heroja (izracunava se na osnovu precnika sprajta)

--- Assets/Scripts/Hero/HeroRadius.cs
+++ Assets/Scripts/Hero/HeroRadius.cs
@@ -6,6 +6,18 @@ public class HeroRadius : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
+		float radius;
+
+		//racunamo poluprecnik na osnovu nacrtanog prefaba (sprite za domet towera)
+		radius = GetComponent<SpriteRenderer> ().bounds.size.x / 2; 
+		//podesavamo radius collidera u zavisnosti od skaliranja
+		if (Mathf.Abs(Mathf.Max(transform.lossyScale.x, transform.lossyScale.y)) != 0){
+			GetComponent<CircleCollider2D> ().radius = radius / Mathf.Abs (Mathf.Max (transform.lossyScale.x, transform.lossyScale.y));
+		}
+		else {
+			GetComponent<CircleCollider2D>().radius = radius;
+		}
+			
 	}
 	
 	// Update is called once per frame
@@ -14,7 +26,7 @@ public class HeroRadius : MonoBehaviour {
 	}
     
 	void OnMouseUp (){
-		transform.gameObject.active = false;
+		//transform.gameObject.active = false;
 	}
     
     /*
