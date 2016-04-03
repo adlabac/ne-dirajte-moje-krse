d633f005c3bdf60166f8acee1408e481b279aa4f
Branko Dujović <brankod@t-com.me>
2016-04-03 14:19:47 +0200

    Dodat na heroju text koji prikazuje level heroja

---
+++ Assets/Scripts/Hero/UpgradeHero.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpgradeHero : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseUp(){
+		GameObject heroParent = transform.parent.parent.gameObject;
+		int heroUp1Price = heroParent.GetComponent<Hero> ().GetUp1Price();
+		ScoreManager.SetCoins(ScoreManager.GetCoins()-heroUp1Price); //podesi broj coina
+	}
+}
