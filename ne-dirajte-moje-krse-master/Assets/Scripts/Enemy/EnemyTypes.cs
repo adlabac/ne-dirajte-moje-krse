786c92a7e693517129f0441a57eb19bfd77b1d8e
Darman7 <nikola.darmanovic7@gmail.com>
2016-04-10 12:48:32 +0200

    Preliminarna verzija pozadine.

---
+++ ne-dirajte-moje-krse-master/Assets/Scripts/Enemy/EnemyTypes.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * 
+ * @author: Nikola Darmanovic
+ *
+*/
+
+using UnityEngine;
+using System.Collections.Generic; //Obavezno ukljucivanje Generic-a ovdje da ne bi imali problem sa razlicitim vrstama elemenata liste
+
+public class EnemyTypes : MonoBehaviour {
+
+
+    public List<EnemyType> enemyType; //Lista koja sadrzi elemente tipa EnemyType
+   
+    //Test Mode
+    /*
+    EnemyType a1 = new EnemyType();
+    EnemyType a2 = new EnemyType();
+    EnemyType r = new EnemyType();
+    */
+    
+    void Start () {
+
+        //Test Metode EnemyType GetByName(string name):
+        /*
+        a1.name = "darman";
+        a2.name = "andrej";
+        enemyType.Add(a1);
+        enemyType.Add(a2);
+        
+        r = GetByName("darman");
+        Debug.Log(r.name);
+        */
+
+	}
+	
+	void Update () {
+	    
+	}
+
+    //Vraca element tipa EnemyType kojeg zovemo samo po imenu
+    public EnemyType GetByName(string name)
+    {
+        return enemyType.Find(x => x.name == name);
+    }
+    
+}
