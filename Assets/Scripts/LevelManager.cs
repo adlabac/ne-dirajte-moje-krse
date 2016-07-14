ee32e42bce18d3357aa7873571960b34670017a9
StevanCakic <stevancakic93@gmail.com>
2016-07-14 19:26:20 +0200

    Dodate nove scene Omogucen prelazak izmedju scena Kreirana klasa LevelManager za prelazak sa scene na scenu

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevelAfter;
+
+	void Start () {
+		if (autoLoadNextLevelAfter <= 0) {
+			Debug.Log ("Level auto load ugasen, unesite pozitivan broj koji predstavlja sekunde");
+		} else {
+			Invoke ("LoadNextLevel", autoLoadNextLevelAfter);
+		}
+	}
+
+	public void LoadLevel(string name){
+		Debug.Log ("Novi level: " + name);
+		Application.LoadLevel (name);
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("Izlaz");
+		Application.Quit ();
+	}
+	
+	public void LoadNextLevel() {
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+}
